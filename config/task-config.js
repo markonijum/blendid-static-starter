083414e134d550bf9daf34302256b16dd577cd9a
markonijum <markonijum@gmail.com>
2018-01-27 14:42:24 +0100

    Initial commit

---
+++ config/task-config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  html        : true,
+  images      : true,
+  fonts       : true,
+  static      : true,
+  svgSprite   : true,
+  ghPages     : true,
+  stylesheets : true,
+
+  javascripts: {
+    entry: {
+      // files paths are relative to
+      // javascripts.dest in path-config.json
+      app: ["./app.js"]
+    }
+  },
+
+  stylesheets: {
+    sass: {
+      includePaths: [
+        './node_modules/susy/sass/',
+        './node_modules/font-awesome/scss/',
+        './node_modules/normalize.css/'
+      ]
+    }
+  },
+
+  browserSync: {
+    server: {
+      // should match `dest` in
+      // path-config.json
+      baseDir: 'public'
+    }
+  },
+
+  production: {
+    rev: true
+  }
+}
